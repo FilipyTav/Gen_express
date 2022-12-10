6573747c22789fd9be9618388095358fdc81fcc1
BiAniTy <drayttc@gmail.com>
2022-12-10 19:48:39 -0300

    Adds setup for the root of the project

---
+++ files/app.ts
@@ -0,0 +1,29 @@
+import express, { Application } from "express";
+
+import path from "path";
+
+import index_router from "./routes/index";
+
+const app: Application = express();
+
+// Allows access to form content
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
+
+const port: Number = 3000;
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
+
+// View engine setup
+app.set("views", path.join(__dirname, "./mvc/views"));
+app.set("view engine", "<----VIEW ENGINE PLACEHOLDER---->");
+
+// Setup static directory
+app.use(express.static(path.join(__dirname, "/../dist")));
+
+app.use("/", index_router);
--- main.sh
+++ main.sh
@@ -6,22 +6,76 @@ readonly target_dir
 base_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 readonly base_dir
 
+compiled=("sass" "scss" "ts")
+readonly compiled
+
 SCRIPT="js"
+STYLESHEETS="css"
 
 usage() { 
-  echo "Usage: $0 [--stylesheet=(css,sass,scss)] [--script=(js,ts,jsx,tsx)]" 1>&2; 
-  exit 1; 
+    echo "Usage: $0 [--stylesheet=(css,sass,scss)] [--script=(js,ts,jsx,tsx)]" 1>&2; 
+    exit 1; 
 }
 
+array_contains () { 
+    local array="$1[@]"
+    local seeking=$2
+
+    local in=1
+    for element in "${!array}"; do
+        if [[ $element == "$seeking" ]]; then
+            in=0
+            break
+        fi
+    done
+    return $in
+}
+
+set_app () {
+    # Options for the build and watch scripts in package.json
+    declare -A build_options
+    build_options["ts"]="tsc -p ."
+    build_options["sass"]="node-sass --include-path sass ./src/styles/styles.css ./dist/styles/styles.$STYLESHEETS"
+
+    local watch_all="nodemon -e $STYLESHEETS,$SCRIPT"
+
+    # If the SCRIPT or STYLESHEETS are compiled, compile them after each change
+    if array_contains compiled "$SCRIPT" || array_contains compiled "$STYLESHEETS"; then
+        watch_all="nodemon -e $STYLESHEETS,$SCRIPT -x 'npm run build'"
+    fi
+
+    # Adds tsconfig if ts is chosen
+    [[ $SCRIPT == "ts" ]] && cp "$base_dir/files/tsconfig.json" "$target_dir"
+
+    # TODO: install packages necessary
+    if [[ $STYLESHEETS == "sass" || $STYLESHEETS == "scss" ]]; then
+        echo
+    fi
+
+    if [[ $SCRIPT == "ts" ]]; then
+        echo
+    fi
+
+}
+
+
 set_src () {
-    mvc=("models" "views" "controllers")
+    local mvc=("models" "views" "controllers")
 
     # Creates a directory for the mvc elements inside src
     for dir in "${mvc[@]}"; do
         mkdir -p "$target_dir/src/mvc/$dir"
     done
 
+    # Creates the routes and the styles dir and initialize them
     mkdir "$target_dir/src/routes" && cp "$base_dir/files/routes/index.$SCRIPT" "$target_dir/src/routes/"
+    mkdir "$target_dir/src/styles" && cp "$base_dir/files/styles/styles.$STYLESHEETS" "$target_dir/src/styles/"
+
+    # Copies the application to the project's src
+    cp "$base_dir/files/app.$SCRIPT" "$target_dir/src/"
+
 }
+
+set_app
 
 set_src
